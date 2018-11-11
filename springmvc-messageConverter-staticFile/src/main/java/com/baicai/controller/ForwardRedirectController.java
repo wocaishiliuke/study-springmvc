f3e171da872a9cc4386d26399ac8b3094d368250
top <top@123.com>
2018-11-11 14:41:23 +0800

    删除多余import

--- springmvc-messageConverter-staticFile/src/main/java/com/baicai/controller/ForwardRedirectController.java
+++ springmvc-messageConverter-staticFile/src/main/java/com/baicai/controller/ForwardRedirectController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
